24a8b211cafdd5c7ae7af4a4c7fd772941bef4f2
SheetJS <dev@sheetjs.com>
2014-05-27 17:26:37 -0400

    updating to 0.6.20

--- shim.js
+++ shim.js
@@ -143,3 +143,11 @@ if (!Array.prototype.map) {
     return A;
   };      
 }
+
+// Based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+
+if (! Array.isArray) {
+    Array.isArray = function(obj) {
+        return Object.prototype.toString.call(obj) === "[object Array]";
+    };
+}
